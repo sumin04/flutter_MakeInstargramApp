9ba4fdf01472f4568230f4bd1c75bdb19c22cfb1
sumin04 <gimsumin259@gmail.com>
2022-12-29 16:04:15 +0900

    firebase 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:get/get.dart';
 
 import 'landing_page.dart';
 import 'package:url_strategy/url_strategy.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
   setPathUrlStrategy();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
